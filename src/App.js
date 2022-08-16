46f73fe75df8e86ec98786f96e314f6d3c9baf02
12BSG12 <carlj8335@gmail.com>
2022-08-17 00:20:30 +0500

    users git commit -m users git commit -m users

--- src/App.js
+++ src/App.js
@@ -6,9 +6,22 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const[users, setUsers] = React.useState([]);
+  const[isLoading, setLoading] = React.useState(true)
+  const[search, setSearch] = React.useState('')
+
+  React.useEffect(() => {
+    fetch('https://reqres.in/api/users').then(response => response.json())
+    .then(userData => setUsers(userData.data)).catch(err => console.log(err)).finally(() => setLoading(false));
+  }, []);
+
+  const handlechange = (e) => {
+    setSearch(e.target.value)
+  }
+
   return (
     <div className="App">
-      <Users />
+      <Users items={users} isLoading={isLoading} search={search} handlechange={handlechange}/>
       {/* <Success /> */}
     </div>
   );
