17749e307cc872d957a4d5714fb22abdd2fb9fd5
12BSG12 <carlj8335@gmail.com>
2022-08-16 21:59:34 +0500

    modal

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import React  from 'react'
+import React from 'react';
 import './index.scss';
 
-function App() {
-  const [count, setCount] = React.useState(0);
-  const onBtnPlus = () => {
-    setCount(count + 1);
+const ModalWindow = ({open, closeModalWindow, children }) => {
+  const show = open ? 'show': '';
+  return(
+    <div className={`overlay animated ${show}`}>
+      <div className="modal">
+        <svg height="200" viewBox="0 0 200 200" width="200" onClick={closeModalWindow}>
+          <title />
+          <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+        </svg>
+        {children} 
+      </div>
+    </div>
+  );
+}
+
+const App = () => {
+  const[open, setOpen] = React.useState(false);
+  const openModalWindow = () => {
+    setOpen(true)
   }
-  const onBtnMinus = () => {
-    setCount(count - 1);
+  const closeModalWindow = () => {
+    setOpen(false)
   }
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{count}</h1>
-        <button className="minus" onClick={onBtnMinus}>- Минус</button>
-        <button className="plus" onClick={onBtnPlus}>Плюс +</button>
-      </div>
+      <button className="open-modal-btn" onClick={openModalWindow}>✨ Открыть окно</button>
+      <ModalWindow open={open} closeModalWindow={closeModalWindow}>
+        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+        <h3>title</h3>
+      </ModalWindow>
     </div>
   );
 }
