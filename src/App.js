c013d7f9222ae4eb675cda1ee57e2647e6561e37
12BSG12 <carlj8335@gmail.com>
2022-08-16 22:11:42 +0500

    users

--- src/App.js
+++ src/App.js
@@ -1,65 +1,15 @@
 import React from 'react';
 import './index.scss';
+import { Success } from './components/Success';
+import { Users } from './components/Users';
 
-const questions = [
-  {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
-    correct: 0,
-  },
-  {
-    title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
-    correct: 1,
-  },
-  {
-    title: 'Что такое JSX?',
-    variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
-    ],
-    correct: 2,
-  },
-];
-
-function Result({correctAnswersCount}) {
-  return (
-    <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали {correctAnswersCount} ответа из {questions.length}</h2>
-      <a href='/'>
-        <button>Попробовать снова</button>
-      </a>
-    </div>
-  );
-}
-
-function Game({step, onClickVariant}) {
-  const percent = Math.round(step / questions.length * 100)
-  return (
-    <>
-      <div className="progress">
-        <div style={{ width: `${percent}%` }} className="progress__inner"></div>
-      </div>
-      <h1>{questions[step].title}</h1>
-      <ul>{questions[step].variants.map((item, index) => <li onClick={() => onClickVariant(index)} key={item}>{item}</li>)}
-      </ul>
-    </>
-  );
-}
+// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
-  const [step, setStep] = React.useState(0);
-  const [correctAnswersCount, setCorrectAnswersCount] = React.useState(0);
-  const onClickVariant = (index) => {
-    if(questions[step].correct === index) setCorrectAnswersCount(correctAnswersCount + 1);
-    setStep(step + 1)
-  }
-  const showQuiz = step !== questions.length ? <Game step={step} onClickVariant={onClickVariant}/>: <Result correctAnswersCount={correctAnswersCount}/>;
   return (
     <div className="App">
-      {showQuiz}
+      <Users />
+      {/* <Success /> */}
     </div>
   );
 }
