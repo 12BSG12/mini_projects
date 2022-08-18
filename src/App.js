094b9ea477be50822f084ae0635e9899461402b1
12BSG12 <carlj8335@gmail.com>
2022-08-18 19:39:57 +0500

    photos

--- src/App.js
+++ src/App.js
@@ -1,48 +1,50 @@
-import React, { useRef, useState } from 'react';
-import { Block } from './Block';
+import React from 'react';
 import './index.scss';
 
+function Collection({ name, images }) {
+  return (
+    <div className="collection">
+      <img className="collection__big" src={images[0]} alt="Item" />
+      <div className="collection__bottom">
+        <img className="collection__mini" src={images[1]} alt="Item" />
+        <img className="collection__mini" src={images[2]} alt="Item" />
+        <img className="collection__mini" src={images[3]} alt="Item" />
+      </div>
+      <h4>{name}</h4>
+    </div>
+  );
+}
+
 function App() {
-  const[fromValue, setFromValue] = useState(0);
-  const[toValue, setToValue] = useState(1);
-  const[fromCurrency, setFromCurrency] = useState('RUB');
-  const[toCurrency, setToCurrency] = useState('USD');
-  const ratesRef = useRef({});
-  React.useEffect(() => {
-    fetch('https://cdn.cur.su/api/cbr.json')
-    .then(response => response.json())
-    .then(data => {
-      ratesRef.current = data.rates;
-      onChangeToPrice(1);
-    }).catch(err  => console.log(err ));
-  }, []);
-  
-  const onChangeFromPrice = (value) => {
-    const price = value / ratesRef.current[fromCurrency]
-    const result = price * ratesRef.current[toCurrency]
-    setToValue(result.toFixed(2))
-    setFromValue(value)
-  }
-
-  const onChangeToPrice = (value) => {
-    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
-    setFromValue(result.toFixed(2))
-    setToValue(value)
-  }
-
-  React.useEffect(() => {
-    onChangeFromPrice(fromValue)
-  }, [fromCurrency]);
-
-  React.useEffect(() => {
-    onChangeToPrice(toValue)
-  }, [toCurrency]);
-   
-  const defaultCurrencies = Object.keys(ratesRef.current);
   return (
     <div className="App">
-      <Block value={fromValue} currency={fromCurrency} onChangeCurrency={setFromCurrency}  onChangeValue={onChangeFromPrice} defaultCurrencies={defaultCurrencies}/>
-      <Block value={toValue} currency={toCurrency} onChangeCurrency={setToCurrency}  onChangeValue={onChangeToPrice} defaultCurrencies={defaultCurrencies}/>
+      <h1>Моя коллекция фотографий</h1>
+      <div className="top">
+        <ul className="tags">
+          <li className="active">Все</li>
+          <li>Горы</li>
+          <li>Море</li>
+          <li>Архитектура</li>
+          <li>Города</li>
+        </ul>
+        <input className="search-input" placeholder="Поиск по названию" />
+      </div>
+      <div className="content">
+        <Collection
+          name="Путешествие по миру"
+          images={[
+            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+          ]}
+        />
+      </div>
+      <ul className="pagination">
+        <li>1</li>
+        <li className="active">2</li>
+        <li>3</li>
+      </ul>
     </div>
   );
 }
